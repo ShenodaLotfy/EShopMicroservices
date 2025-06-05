1d3e7cad1ab2c715ce3fb102274a0bff851c9f5f
Shenodalotfy <shenodalotfy1@gmail.com>
2025-06-05 11:57:57 +0300

    docker compose added, added postgres container and database connection configuration

--- src/Service/Catalog/Catalog.API/Program.cs
+++ src/Service/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions(); // using light weight session for read/write operations
 
 var app = builder.Build();
 
