23e17aee488f826c6dc42e18b92eb96585e75d2d
Shenodalotfy <shenodalotfy1@gmail.com>
2025-06-05 11:05:51 +0300

    Added Catalog.API with product feature, added building blocks class library with abstractions and nuget libraries,

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    // Not returning a response, Unit represents a void 
+    public interface ICommand : IRequest<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
