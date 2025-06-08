249c18fb32d7b993d3de81a99e6b456175c72890
Shenodalotfy <shenodalotfy1@gmail.com>
2025-06-08 05:15:57 +0300

    Martin pagination

--- src/Service/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Service/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,14 +1,14 @@
 ﻿
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int PageIndex = 1, int PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     internal class GetProductsQueryHandler(IDocumentSession session) : 
         IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(query.PageIndex, query.PageSize, cancellationToken);
             return new GetProductsResult(products); 
         }
     }
