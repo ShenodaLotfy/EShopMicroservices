4e93b369206caff9afd0015ba62fffd182e01865
Shenodalotfy <shenodalotfy1@gmail.com>
2025-07-25 11:32:31 +0300

    Add Basket API endpoints and handlers, Added Carter and Marten libraries, Docker-compose configuration

---
+++ src/Service/Basket.API/GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using BuildingBlocks.CQRS;
+global using Basket.API.Models;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using Basket.API.Exceptions;
+global using Marten;
+global using Basket.API.Data;
--- src/Service/Basket.API/Program.cs
+++ src/Service/Basket.API/Program.cs
@@ -1,5 +1,25 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database"));
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
