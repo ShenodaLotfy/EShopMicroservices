261a55110840f0479b8394e3f4ad812a75379fa6
Shenodalotfy <shenodalotfy1@gmail.com>
2025-07-26 06:52:48 +0300

    Add DbContext to Discount.Grpc

---
+++ src/Service/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                    new Coupon { Id = 1, ProductName = "IPhone", Description = "IPhone Discount", Amount = 100},
+                    new Coupon { Id = 2, ProductName = "Samsung", Description = "Samsung Discount", Amount = 100}
+                );
+        }
+    }
+}
---
+++ src/Service/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder appBuilder)
+        {
+            var scope = appBuilder.ApplicationServices.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dbContext.Database.MigrateAsync();
+
+            return appBuilder;
+        }
+    }
+}
